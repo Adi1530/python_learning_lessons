b8fb2b849553210acdf81fb20d571b2883a29e54
Chandra Aditya Ramesh <chandraaditya.r@zohointern.com>
2026-01-05 10:48:13 +0530

    set_2

---
+++ set/set_2.py
@@ -0,0 +1,13 @@
+nums={1,2,3,4,5,6}
+nums_rest={7,8,6,9,10,11,12}
+union_set=nums.union(nums_rest)
+print(union_set)
+
+
+set_1={1,2,3,4,5}
+set_2={2,3,4}
+print(set_1.intersection(set_2))
+
+print(set_2.issubset(set_1))
+print(set_1.issuperset(set_2))
+
